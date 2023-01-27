937cc33310d0e3127988a9226a792b97abf69a8a
wx <251875969@qq.com>
2023-01-28 07:06:11 +0800

    数据库调试

--- dapp/pages/api/task.ts
+++ dapp/pages/api/task.ts
@@ -30,7 +30,37 @@ async function getFixtures(leagueId: number, season: string, date: string) {
     return response.data.response
 }
 
+async function getOdds(leagueId: number, season: string, date: string) {
+    // 获取赔率
+    const response = await http.get("/fixtures", {
+        params: {
+            league: leagueId,
+            status: "NS",
+            season,
+            date,
+        },
+    })
+    // console.log(response.data)
+
+    return response.data.response
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const auth =
+        "Basic " +
+        Buffer.from(
+            process.env["INFURA_IPFS_ID"] + ":" + process.env["INFURA_IPFS_SECRECT"],
+        ).toString("base64")
+
+    const client = ipfsClient.create({
+        host: "ipfs.infura.io",
+        port: 5001,
+        protocol: "https",
+        headers: {
+            authorization: auth,
+        },
+    })
+
     const coreABI = JSON.parse(
         fs.readFileSync("./abi/contracts/interfaces/ICore.sol/ICore.json", "utf8"),
     )
@@ -47,8 +77,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     const leagues = await prisma.league.findMany()
 
-    for (const leagueId of leagues) {
-        const fixtures = await getFixtures(leagueId.apiId, "2022", "2023-01-24")
+    // 数据库或 infura 的接口被墙
+    // 所以本地调试需要设置代理
+    if (process.env.NODE_ENV === "development") {
+        proxy.setConfig(process.env.PROXY)
+        proxy.start()
+    }
+
+    for (const league of leagues) {
+        const fixtures = await getFixtures(league.apiId, "2022", "2023-01-28")
         for (const fixture of fixtures) {
             if (
                 (await prisma.game.findFirst({
@@ -57,47 +94,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                     },
                 })) === null
             ) {
-                const data = JSON.stringify({
-                    test: "test",
-                })
-
                 // 数据库中没有 game
-                // 新建 game 至数据库和合约
+                // 1. 详细保存至 IPFS。
+                // 2. 创建 game 至合约。
+                // 3. 新建记录至数据库。
+
+                // 1. 详细保存至 IPFS。
+
+                // js-multiformats
+
+                const data = JSON.stringify(fixture)
+
+                const result = await client.add(data, { pin: true })
+
+                // 2. 创建 game 至合约。
                 await core.createGame(
                     Date.now(),
                     Date.now() + 60 * 60,
-                    utils.formatBytes32String("path".substring(2)),
+                    result.cid.toV0().multihash.bytes.slice(2),
                 )
 
-                console.log(await core.lastGameId())
-                if (process.env.NODE_ENV === "development") {
-                    proxy.setConfig(process.env.PROXY)
-                    proxy.start()
-                }
+                const gameId = (await core.lastGameId()).toNumber()
+                console.log(gameId)
 
-                const auth =
-                    "Basic " +
-                    Buffer.from(
-                        process.env["INFURA_IPFS_ID"] + ":" + process.env["INFURA_IPFS_SECRECT"],
-                    ).toString("base64")
-
-                const client = ipfsClient.create({
-                    host: "ipfs.infura.io",
-                    port: 5001,
-                    protocol: "https",
-                    headers: {
-                        authorization: auth,
+                // 3. 记录至数据库。
+                const game = await prisma.game.create({
+                    data: {
+                        ipfs: result.path,
+                        apiId: fixture.fixture.id,
+                        leagueId: league.id,
                     },
                 })
+                console.log(game)
 
-                const result = await client.add(data)
-                console.log(result)
-
-                if (process.env.NODE_ENV === "development") {
-                    proxy.stop()
-                }
+                // 创建 gameble，比如输赢平，进球，之类
             }
         }
     }
+
+    if (process.env.NODE_ENV === "development") {
+        proxy.stop()
+    }
+
     res.status(200).json({})
 }
